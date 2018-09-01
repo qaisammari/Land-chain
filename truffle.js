ac7a90c976585c25899c29a98b6a961896878bc9
Qais Ammari <qais.ammari@triphop.com>
2018-09-01 10:52:22 +0300

    FIXED SOME ISSUE ON THE LINKERS

--- truffle.js
+++ truffle.js
@@ -3,10 +3,10 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      from: '0xD87a4D99EDd08Fde1F461D4FF29C13181177d285',
+      from: '0xd4e3e671053af5980e4235b9d46172b0485947a3',
       network_id: 5777, // Match any network id
        gasPrice: 50,   // <--- Twice as much
-       gas: 99999999,   // <--- Twice as much
+       gas: 6712390,   // <--- Twice as much
     }
   }
 };
